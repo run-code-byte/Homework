a97b23c9250d05576d645eb173525d6eb5340cb8
Apubhub <d82be7925105df2121c78c4be02deea7>
2026-08-24 20:28:50 +0800

    8月24号，02阶段winform，day01作业

---
+++ 02阶段Winform/day01/day01/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace day01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(102, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 20);
+            label1.TabIndex = 0;
+            label1.Text = "用户名";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(102, 130);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 20);
+            label2.TabIndex = 0;
+            label2.Text = "密码";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(102, 236);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 20);
+            label3.TabIndex = 0;
+            label3.Text = "性别";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(102, 180);
+            label4.Name = "label4";
+            label4.Size = new Size(69, 20);
+            label4.TabIndex = 0;
+            label4.Text = "确认密码";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(102, 290);
+            label6.Name = "label6";
+            label6.Size = new Size(39, 20);
+            label6.TabIndex = 0;
+            label6.Text = "爱好";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(278, 353);
+            button1.Name = "button1";
+            button1.Size = new Size(180, 37);
+            button1.TabIndex = 1;
+            button1.Text = "提交";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(173, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(385, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(173, 127);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(385, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(173, 177);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(385, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.Font = new Font("微软雅黑", 15F, FontStyle.Bold, GraphicsUnit.Point, 134);
+            label5.Location = new Point(300, 29);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 35);
+            label5.TabIndex = 3;
+            label5.Text = "注册";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(467, 234);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(60, 24);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "未知";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(321, 234);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(45, 24);
+            radioButton3.TabIndex = 4;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "女";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(178, 234);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(45, 24);
+            radioButton4.TabIndex = 4;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "男";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(526, 289);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(55, 24);
+            checkBox1.TabIndex = 5;
+            checkBox1.Text = "rap";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(406, 289);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(61, 24);
+            checkBox2.TabIndex = 5;
+            checkBox2.Text = "篮球";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(293, 289);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(61, 24);
+            checkBox3.TabIndex = 5;
+            checkBox3.Text = "跳舞";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(173, 289);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(61, 24);
+            checkBox4.TabIndex = 5;
+            checkBox4.Text = "唱歌";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(685, 460);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(radioButton4);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton3);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label6;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label5;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+    }
+}
