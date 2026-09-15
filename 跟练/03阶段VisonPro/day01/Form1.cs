1f0d9d059dadaadf66b436518767d2de17c64ee2
Apubhub <d82be7925105df2121c78c4be02deea7>
2026-09-15 22:56:59 +0800

    9月15号，03阶段visonpro，day02

---
+++ 跟练/03阶段VisonPro/day01/Form1.cs
@@ -0,0 +1,51 @@
+﻿using Cognex.VisionPro;
+using Cognex.VisionPro.ImageFile;
+using Cognex.VisionPro.ToolBlock;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog OFD = new OpenFileDialog())
+            {
+                OFD.Title = "请选择检测轮胎数量的图片";
+                OFD.Filter = "图片|*.jpg;*.jpeg;*.png;*.gif;";
+                if (OFD.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(OFD.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    CogImageFileTool CFT = new CogImageFileTool();
+                    CFT.Operator.Open(OFD.FileName, CogImageFileModeConstants.Read);
+                    CFT.Run();
+
+                    string VppFilePath = Path.Combine(Directory.GetCurrentDirectory(), "vpps", "玩具轮胎的计数方案.vpp");
+                    object LOF = CogSerializer.LoadObjectFromFile(VppFilePath);
+
+                    CogToolBlock CTB = (LOF as CogToolBlock);
+                    CTB.Inputs["OutInputImage"].Value = CFT.OutputImage;
+                    CTB.Run();
+
+                    //MessageBox.Show(CTB.Outputs["Results_Count"].Value.ToString());
+                    label2.Text = CTB.Outputs["Results_Count"].Value.ToString();
+                }
+            }
+        }
+    }
+}
